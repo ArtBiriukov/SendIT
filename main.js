405fb5a583c9af79223396ebbba77d09384ca806
Artem <carbon0093@gmail.com>
2021-04-12 00:03:03 +0300

    start JS

---
+++ main.js
@@ -0,0 +1,16 @@
+let btnOpen = document.getElementById('modal-open');
+let modal = document.getElementById('modal-type');
+
+let overlay = document.getElementById('overlay');
+let btnClose = document.getElementById('modal-close');
+
+btnOpen.addEventListener('click',function(){
+    modal.classList.add('modal__active');
+});
+
+function closeModal(){
+    modal.classList.remove('modal__active');
+}
+
+overlay.addEventListener('click',closeModal);
+btnClose.addEventListener('click',closeModal);
